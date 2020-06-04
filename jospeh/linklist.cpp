46977b1e7c1961b3d42e59d94e45151cc6641818
jiuyoupp <529391453@qq.com>
2020-06-04 15:06:14 +0800

    读文件并完成字符数字转换

--- jospeh/linklist.cpp
+++ jospeh/linklist.cpp
@@ -58,10 +58,10 @@ int GetFileLin(FILE* file)
 		++lin;
 	}
 	//将光标移动到文件开始 否则下次读取该文件时会从文件末尾开始读
+	
 	fseek(file, 0, SEEK_SET);
 	return lin;
 }
-
 
 void GetFile(FILE* file, char** pArr, int lin)
 {
@@ -81,7 +81,6 @@ void GetFile(FILE* file, char** pArr, int lin)
 	}
 	// 创建缓冲区
 	char buf[N] = { 0 };
-
 	int index = 0;
 
 	while (fgets(buf, N, file) != NULL)
@@ -90,7 +89,6 @@ void GetFile(FILE* file, char** pArr, int lin)
 		int lindate = strlen(buf) + 1;
 		//给当前行分配内存
 		char* plindate = (char*)malloc(sizeof(char) * lindate);
-
 		//将行数据拷贝到空间中
 		strcpy(plindate, buf);
 		pArr[index++] = plindate;
@@ -98,14 +96,55 @@ void GetFile(FILE* file, char** pArr, int lin)
 	}
 }
 
-void fileshow(char** pArr, int len)
+void FileShow(char** pArr, int len)
 {
 	for (int i = 0; i < len; ++i)
 	{
 		printf("%s", pArr[i]);
 	}
 }
-char** Reader()
+int CharToInt(char* str)
+{
+	int i = 0;
+	while (*str!='\n')
+	{
+		if (i==0)
+		{
+			i+=(int)(*str-'0');
+			str++;
+		}
+		else 
+		{
+			i = i * 10+(int)((*str) - '0');
+			str++;
+		}
+	}
+	return i;
+
+}
+Person** GetData(char** pArr, int len)
+{
+	Person** reader = (Person**)malloc(sizeof(Person*) * len);
+	
+	int age = 0;
+	for (int i = 0; i < len; ++i)
+	{
+
+		char* token;
+		reader[i] =(Person *) malloc(sizeof(Person));
+		reader[i]->name = (char*)malloc(sizeof(char) * 12 + 1);
+		token = strtok(*(pArr+i), " ");
+		strcpy(reader[i]->name, token);
+		token = strtok(NULL, "\0");
+		age = CharToInt(token);
+		reader[i]->age = age;
+		//reader[i]->age = *(token) -'0';
+	/*	printf("%s\n", reader[i]->name);
+		printf("%d\n",age);*/
+	}
+	return reader;
+}
+Person** Reader()
 {
 	FILE* file = fopen("D:\\Ccode\\jospeh\\peopledata.txt", "r");
 	if (file == NULL)
@@ -114,39 +153,82 @@ char** Reader()
 		return NULL;
 	}
 	//统计文件行数
+
 	int lin = GetFileLin(file);
 	//开辟指针数组分别指向数据
 	char** pArr = (char**)malloc(sizeof(char*) * lin);
+	memset(pArr, 0, lin);
 	GetFile(file, pArr, lin);
-	//char pp[N] = "cccccccccc";
-	//serfile(pArr, lin, pp);
-	fileshow(pArr, lin);
+	Person** reader;
+	reader = GetData(pArr,lin);
+	//FileShow(pArr, lin);
 	printf("%d", lin);
 	fclose(file);
-	return pArr;
+	return reader;
 }
+
+
+
 // L指向尾结点
 Ring InitList()
 {
 	int i;
 	Ring L = (Ring)malloc(sizeof(Node));
 	if (!L) // 存储分配失败
+	{
 		exit(OVERFLOW);
+	}
 	L->next = L; // 指针域为空
 	L->length = 0;
 	return L;
 
 }
 
-
 Status ListEmpty(Ring L)
 { // 初始条件：线性表L已存在。操作结果：若L为空表，则返回TRUE，否则返回FALSE
 	if (L->next != L) // 非空
+	{
 		return FALSE;
+	}
 	else
+	{
 		return TRUE;
+	}
 }
-Status RingAppend(Ring L, Person q)
+
+Status FromReader(Ring L, Person** reader,int lin)
+{
+
+	for (int i = 0; i < lin; ++i)
+	{
+		if (L->next->length == 0)
+		{
+
+			L->person.name = (char*)malloc(sizeof(char) * 12 + 1);
+			strcpy(L->person.name, reader[i]->name); // 插入L中
+			L->person.age = reader[i]->age;
+			L->next->length++;
+		}
+
+		else
+		{
+			Ring s;
+			Ring head = L->next;
+			s = (Ring)malloc(sizeof(Node)); // 生成新结点
+			s->person.name = (char*)malloc(sizeof(char) * 12 + 1);
+			s->person.age = reader[i]->age;
+			strcpy(s->person.name, reader[i]->name); // 插入L中
+			//s->person = q; 结构体中有堆内存不能使用该方法否则释放内存时可能出错
+			s->next = head;
+			L->next = s;
+			L = s;
+			L->next->length++;
+		}
+	}
+	return OK;
+
+}
+Status Append(Ring L, Person q)
 {
 	if (L->length == 0)
 	{
@@ -169,7 +251,8 @@ Status RingAppend(Ring L, Person q)
 		s->next = head;
 		L->next = s;
 		L = s;
-		L->length++;
+		//头指针保存链表长度
+		L->next->length++;
 	}
 	return OK;
 
@@ -177,15 +260,19 @@ Status RingAppend(Ring L, Person q)
 
 int ListLength(Ring L)
 {// 初始条件：线性表L已存在。操作结果：返回L中数据元素个数
-	return L->length;
+	return L->next->length;
 }
 
 bool IsEmpty(Ring L)
 {
-	if (L->length)
+	if (L->next->length)
+	{
 		return false;
+	}
 	else
+	{
 		return true;
+	}
 }
 
 Status ListDelete(Ring L, int pos, Person *qq)
@@ -195,36 +282,43 @@ Status ListDelete(Ring L, int pos, Person *qq)
 	Ring s = NULL;
 	int j = 0;
 	if (pos <= 0 || pos > ListLength(L)) // 第i个元素不存在
+	{
 		return ERROR;
+	}
 	while (j < pos - 1) // 寻找第i-1个结点
 	{
 		p = p->next;
 		j++;
 	}
 	q = p->next; // q指向待删除结点
-	*qq = q->person;
+	qq->age = q->person.age;
+	qq->name = (char*)malloc(sizeof(char) * 12 + 1);
+	qq->name = q->person.name;
 	p->next = q->next;
+	printf("name%s,age%d",qq->name,qq->age);
 	if (L == q) // 删除的是表尾元素
+	{
 		L = p;
+	}
 	free(q); // 释放待删除结点
 	q = NULL;
-	L->length--;
+	L->next->length--;
 	return OK;
 
 }
 
 
-Person NextPerson(Ring L,int step,int location)
+Person* NextPerson(Ring L,int step,int location)
 {
 	if (IsEmpty(L) != false)
 	{
 		printf("环为空");
 	}
-	int size = L->length;
+	int size = L->next->length;
 	int id = (location - 1) % size;
 	id = (id + (step - 1)) % size;
-	Person outperson;
-	ListDelete(L, id, &outperson);
+	Person* outperson=NULL;
+	ListDelete(L, id, outperson);
 	return outperson;
 
 
@@ -259,38 +353,44 @@ void FreeSpace(Ring L)
 }
 int main(void)
 {
-	Person P1;
-	Person P2;
+	
 	Ring jospeh=NULL;
-	Ring pp = NULL;
-	P1.age = 13;
-	P2.age = 19;
-	P1.name = (char*)malloc(sizeof(char) * 12);
-	strcpy(P1.name,"sddsa" );
-	P2.name = (char*)malloc(sizeof(char) * 12);
-	strcpy(P2.name, "dkkk");
-	jospeh = InitList();
-	printf("2222\n");
-	RingAppend(jospeh, P1);
-	RingAppend(jospeh, P2);
-	//第一个指针没有存放变量值
-	//第一个尾指针
-	printf("%d\n", jospeh->person.age);
-	printf("%s\n", jospeh->person.name);
-	//第二个尾指针
-	printf("%d\n", jospeh->next->person.age);
-	printf("%s\n", jospeh->next->person.name);
-	char nan;
+	//Ring pp = NULL;
+	//P1.age = 13;
+	//P2.age = 19;
+	//P1.name = (char*)malloc(sizeof(char) * 12);
+	//strcpy(P1.name,"sddsa" );
+	//P2.name = (char*)malloc(sizeof(char) * 12);
+	//strcpy(P2.name, "dkkk");
+
+	//printf("2222\n");
+	//Append(jospeh, P1);
+	//Append(jospeh, P2);
+	////第一个指针没有存放变量值
+	////第一个尾指针
+	//printf("%d\n", jospeh->person.age);
+	//printf("%s\n", jospeh->person.name);
+	////第二个尾指针
+	//printf("%d\n", jospeh->next->person.age);
+	//printf("%s\n", jospeh->next->person.name);
+	/*char nan;
 	int age;
 	Person qq;
 	
 	NextPerson(jospeh, 2, 3);
-	NextPerson(jospeh, 2, 3);
+	NextPerson(jospeh, 2, 3);*/
 	//Person qq ;
 	//ListDelete(jospeh, 1,&qq);
 	//printf("%s", qq.name);
-	char** sss;
+	jospeh = InitList();
+	Person** sss;
 	sss=Reader();
+	FromReader(jospeh,sss,10);
+	
+	NextPerson(jospeh, 2, 3);
+	NextPerson(jospeh, 2, 3);
+	NextPerson(jospeh, 2, 3);
+	NextPerson(jospeh, 2, 3);
 	FreeSpace(jospeh);
 
 	system("pause");
