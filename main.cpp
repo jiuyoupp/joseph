1384f8977953eb0cf56019736ad8e16257a3cbb9
jiuyoupp <529391453@qq.com>
2020-06-11 00:58:46 +0800

    对main函数及json文件的修改

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include"jospeh.h"
 
-
 int main()
 {
 	Ring* ring = NULL;
 	ring = from_reader();
-	Person * outsequence;
+	Person * outsequence=NULL;
 	reset(ring, 3, 3);
+	/*next(ring);
 	next(ring);
 	next(ring);
 	next(ring);
@@ -16,9 +16,9 @@ int main()
 	next(ring);
 	next(ring);
 	next(ring);
-	next(ring);
-	next(ring);
-	printf("finish");
+	next(ring);*/
+	printf("finish\n");
+	free_jospeh(ring);
 	system("pause");
 	return EXIT_SUCCESS;
 }
